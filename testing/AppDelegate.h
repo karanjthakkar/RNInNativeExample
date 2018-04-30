61203858f8fbd67c916ce3a24ee0bad1b8a269a3
Karan Thakkar <karan.thakkar@skyscanner.net>
2018-04-30 17:33:50 +0100

    Initial Commit

---
+++ testing/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  testing
+//
+//  Created by Karan Thakkar on 30/04/2018.
+//  Copyright © 2018 Karan Thakkar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ testing/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  testing
+//
+//  Created by Karan Thakkar on 30/04/2018.
+//  Copyright © 2018 Karan Thakkar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
